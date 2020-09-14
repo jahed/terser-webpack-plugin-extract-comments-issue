04b06322b0d35836000d1bf51e37226e46f11a0d
Jahed <jahed.public@gmail.com>
2020-09-14 15:21:21 +0100

    fix: woops, unfinished line

--- test.sh
+++ test.sh
@@ -8,9 +8,4 @@ for i in `seq 5`; do
     npm run build
 done
 
-for i in $(ls output); do
-  comment_line="$(cat "output/${i}/*.js" | tail -n1)"
-  if [[ "$comment_line" !== ]]
-done
-
 echo "Done. Look at ./outputs."
